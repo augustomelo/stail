cc6303aff3fbc1d538450d7e127d828bea64a374
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2024-08-05 15:30:47 +0100

    refactor: using struct and iterfaces

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 // API docs https://docs.datadoghq.com/api/latest/logs/?code-lang=go#get-a-list-of-logs
@@ -14,53 +16,108 @@ import (
 // request object https://github.com/DataDog/datadog-api-client-go/blob/8f7bfb291511b4fa689c2fc0c2826d75b731e3c8/api/datadogV2/api_logs.go#L268
 
 // LogsStorageTier Specifies storage type as indexes, online-archives or flex
+
+// The idea is to have a Producer which will grab the data from a API for example,
+// and a conrumerwhich will grab the data from the Producer and tranform into
+// something
+
+type Integration string
+
 const (
-	DATADOG_TIME_ASCENDING      string = "timestamp"
-	DATADOG_TIME_DESCENDING     string = "-timestamp"
 	DATADOG_API_KEY             string = "DD-API-KEY"
 	DATADOG_APPLICATION_KEY     string = "DD-APPLICATION-KEY"
 	DATADOG_ENV_API_KEY         string = "DD_API_KEY"
 	DATADOG_ENV_APPLICATION_KEY string = "DD_APPLICATION_KEY"
 
-	HTTP_HEADER_ACCEPT string = "Accept"
+	DATADOG_QUERY_FILTER_QUERY    string = "filter[query]"
+	DATADOG_QUERY_PAGE_LIMIT      string = "page[limit]"
+	DATADOG_QUERY_SORT            string = "sort"
+	DATADOG_QUERY_SORT_ASCENDING  string = "timestamp"
+	DATADOG_QUERY_SORT_DESCENDING string = "-timestamp"
+
+	HTTP_HEADER_ACCEPT           string = "Accept"
+	HTTP_HEADER_APPLICATION_JSON string = "application/json"
+
+	DATADOG Integration = "DATADOG"
 )
 
-func main() {
-	fmt.Println("Start")
-	fetchLogs()
-	fmt.Println("End")
+type Producer interface {
+	Produce(context.Context) (string, error)
 }
 
-func fetchLogs() {
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodGet, "https://api.datadoghq.eu/api/v2/logs/events", nil)
-	if err != nil {
-		fmt.Print(err)
-		return
+type DataDogSource struct {
+	Headers http.Header
+	URL     *url.URL
+	Client  *http.Client
+	Type    Integration
+}
+
+// Read from XDG ?
+func buildDataDogSource() *DataDogSource {
+	ddSource := &DataDogSource{
+		Headers: http.Header{
+			HTTP_HEADER_ACCEPT:      {HTTP_HEADER_APPLICATION_JSON},
+			DATADOG_API_KEY:         {os.Getenv(DATADOG_ENV_API_KEY)},
+			DATADOG_APPLICATION_KEY: {os.Getenv(DATADOG_ENV_APPLICATION_KEY)},
+		},
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   "api.datadoghq.eu",
+			Path:   "/api/v2/logs/events",
+		},
+		Client: &http.Client{
+			Timeout: time.Duration(1) * time.Second,
+		},
+		Type: DATADOG,
 	}
 
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add(DATADOG_API_KEY, os.Getenv(DATADOG_ENV_API_KEY))
-	req.Header.Add(DATADOG_APPLICATION_KEY, os.Getenv(DATADOG_ENV_APPLICATION_KEY))
+	slog.Debug("Built DataDogSource", "source", ddSource)
 
-	req.URL.RawQuery = url.Values{
-		"filter[query]": []string{""},
-		"sort":          []string{DATADOG_TIME_ASCENDING},
-		"page[limit]":   []string{"1"},
-	}.Encode()
+	return ddSource
+}
 
-	resp, err := client.Do(req)
+func (source DataDogSource) Produce(context.Context) (string, error) {
+	query := source.URL.Query()
+	query.Set(DATADOG_QUERY_FILTER_QUERY, "")
+	query.Set(DATADOG_QUERY_SORT, DATADOG_QUERY_SORT_ASCENDING)
+	query.Set(DATADOG_QUERY_PAGE_LIMIT, "1")
+	source.URL.RawQuery = query.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, source.URL.String(), nil)
 	if err != nil {
-		fmt.Print(err)
-		return
+		slog.Error("Error while creating request", "err", err)
+		return "", err
+	}
+
+	req.Header = source.Headers
+
+	resp, err := source.Client.Do(req)
+	if err != nil {
+		slog.Error("Error while performing the request", "err", err)
+		return "", err
 	}
 
 	defer resp.Body.Close()
 	bodyResponse, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Print(err)
-		return
+		slog.Error("Error while reading the response", "err", err)
+		return "", err
 	}
 
-	fmt.Println(string(bodyResponse))
+	slog.Debug("Body resposse", "body", bodyResponse)
+	return string(bodyResponse), err
+}
+
+func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	slog.Debug("Start")
+
+	source := buildDataDogSource()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	source.Produce(ctx)
+
+	slog.Debug("End")
 }
